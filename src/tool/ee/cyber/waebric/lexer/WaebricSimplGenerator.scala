3a37b8a61febe53b95c923900e5f5678ee5c62ed
David Pugal <david.pugal@gmail.com>
2011-01-24 01:38:43 -0800

    99 out of 100 tests done and checked. Some fixes

--- src/tool/ee/cyber/waebric/lexer/WaebricSimplGenerator.scala
+++ src/tool/ee/cyber/waebric/lexer/WaebricSimplGenerator.scala
@@ -1,19 +1,22 @@
 package ee.cyber.waebric.lexer;
 
 import ee.cyber.simplicitas.{GeneratorBase, MainBase}
+import Dump._
+
 
 class WaebricSimplGenerator(destDir: String)
         extends GeneratorBase(destDir) {
 //  val templates = getTemplates("WaebricSimpl.stg")
 
   def generate(tree: Program) {
-      println(tree)
-    val args = tree.toJavaMap()
-//    writeFile("TestOutput.java", templates.getInstanceOf("program", args))
+      val dumped = Dump.dumpNode(tree)
+      print(dumped);
+      println("RAW:")
+      print(tree);
   }
 }
 
-object WaebricSimplMain extends MainBase {
+object WaebricSimplAST extends MainBase {
   def main(argv: Array[String]) {
     parseOptions(argv)
     val grammar = new WaebricSimplGrammar()
