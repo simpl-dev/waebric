360ce7132f4d10ce2e069c58ed0ff7df131e17ba
David Pugal <david.pugal@gmail.com>
2011-02-22 16:15:27 -0800

    Node based XML building...

--- src/tool/ee/cyber/waebric/codegenerator/Generator.scala
+++ src/tool/ee/cyber/waebric/codegenerator/Generator.scala
@@ -5,6 +5,8 @@ import collection.mutable.Map
 //import xml.Elem
 //import xml.XML
 import org.xml.sax._
+import xml._
+
 
 import ee.cyber.simplicitas.{GeneratorBase, MainBase, CommonNode, SourceMessage}
 import ee.cyber.simplicitas.PrettyPrint._
@@ -15,9 +17,10 @@ private class Generator(tree: Program) {
 
   val errors = ArrayBuffer[SourceMessage]()
   var buffer: StringBuffer = new StringBuffer
+  var nodes: NodeSeq = List.empty
+
 
   def generate() {
-    import xml._
 
     val globalEnv: Env = new Env(Map.empty, Map.empty)
 
@@ -26,11 +29,17 @@ private class Generator(tree: Program) {
     println("processDefs result: " + globalEnv)
 
     // todo: first process sites. if not found, try with "main"
+    nodes = List.empty
     generate(globalEnv.defs.get("main").get, globalEnv);
-    println(buffer.toString)
+    println(nodes.toString)
 
+
+
+    /*
     var outputXml: Elem = null
     var outputText: String = null
+
+
     try {
       outputXml = XML.loadString(buffer.toString)
     } catch {
@@ -53,6 +62,7 @@ private class Generator(tree: Program) {
       println("Raw output")
       println(outputText)
     }
+    */
 
 
 
@@ -87,18 +97,6 @@ private class Generator(tree: Program) {
         processStatements(statements, env)
       case FunctionDef(FunctionName(idCon), statements, _) =>
         processStatements(statements, env)
-      case EchoStatement(echoBody, _) =>
-        println("EchoStatement found")
-        generate(echoBody, env)
-        buffer.append("\n")
-      case CatExpression(left, right) =>
-        generate(left, env)
-        generate(right, env)
-      case Text(text) =>
-        buffer.append(stripQuote(text))
-      case NatCon(text) =>
-        buffer.append(text)
-
       case _ => ()
     }
 
@@ -107,10 +105,46 @@ private class Generator(tree: Program) {
   def processStatements(statements: List[Statement], env: Env) {
     for (statement <- statements) {
       generate(statement, env)
+      statement match {
+        case EchoStatement(echoBody, _) =>
+          val ret = processExpressions(echoBody, env)
+          println("EchoStatement returned: " + ret.toString)
+
+        case _ => ()
+
+      }
+
     }
   }
 
-  def stripQuote(s: String) = s.substring(1, + s.length - 1)
+  //def processExpressions(exp: CommonNode, env: Env) : NodeSeq = processExpressions(exp, env, null)
+
+  def processExpressions(exp: CommonNode, env: Env) : NodeSeq = {
+    exp match {
+      /* Terminal handling */
+      case Txt(text) => Text(stripQuote(text))
+      case NatCon(text) => Text(text)
+      case PreText(text) => Text(stripPre(text))
+      case PostText(text) => Text(stripPost(text))
+
+
+      /* Expression handling */
+      case CatExpression(left, right) =>
+        processExpressions(left, env) ++ processExpressions(right, env)
+      case Embedding(preText, embed, textTail) =>
+        processExpressions(preText, env) ++ processExpressions(embed, env) ++
+            processExpressions(textTail, env)
+      case _ => Text("")
+    }
+
+  }
+
+  def textElem(name: String, text: String) =  Elem(null, name, Null, TopScope, Text(text))
+  //def generalElem(name: String, child: Node*) = new Elem(null, name, Null, TopScope, child)
+
+  def stripQuote(s: String) = s.substring(1, s.length - 1)
+  def stripPre(s: String) = s.substring(1, s.length)
+  def stripPost(s: String) = s.substring(0, s.length - 1)
   def initHtml = """<?xml version="1.0" encoding="UTF-8"?>""" + "\n" + "<html>" + "\n"
   def closeHtml= "</html>"
 
